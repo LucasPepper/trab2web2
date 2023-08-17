9a2aa16da9bb1937c1d40c34bfd84998036d4dc6
LucasPepper <pimentalucas@hotmail.com>
2023-08-17 15:55:08 -0300

    Sessions

--- gerenciamento_alunos/settings.py
+++ gerenciamento_alunos/settings.py
@@ -106,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
